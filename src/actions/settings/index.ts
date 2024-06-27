6b13d608a8fe179d85c2fc14427f9e5169568d5d
Ivan Dujaut <dujautivan@gmail.com>
2024-06-27 12:35:15 -0300

    feat: Add Accordion component and BotTrainingForm

--- src/actions/settings/index.ts
+++ src/actions/settings/index.ts
@@ -321,3 +321,129 @@ export const onDeleteUserDomain = async (id: string) => {
     console.error(error);
   }
 };
+
+export const onCreateHelpDeskQuestion = async (id: string, question: string, answer: string) => {
+  try {
+    //ad the quuestion and get all the questions
+    const hekpDeskQuestion = await client.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        helpdesk: {
+          create: {
+            question,
+            answer,
+          },
+        },
+      },
+      include: {
+        helpdesk: {
+          select: {
+            id: true,
+            question: true,
+            answer: true,
+          },
+        },
+      },
+    });
+
+    if (hekpDeskQuestion) {
+      return {
+        status: 200,
+        message: "New help desk question added",
+        questions: hekpDeskQuestion.helpdesk,
+      };
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const onGetAllHelpDeskQuestions = async (id: string) => {
+  try {
+    const questions = await client.helpDesk.findMany({
+      where: {
+        domainId: id,
+      },
+      select: {
+        question: true,
+        answer: true,
+        id: true,
+      },
+    });
+
+    return {
+      status: 200,
+      message: "New help desk question added",
+      questions: questions,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const onCreateFilterQuestions = async (id: string, question: string) => {
+  try {
+    const filterQuestion = await client.domain.update({
+      where: {
+        id,
+      },
+      data: {
+        filterQuestions: {
+          create: {
+            question,
+          },
+        },
+      },
+      include: {
+        filterQuestions: {
+          select: {
+            id: true,
+            question: true,
+          },
+        },
+      },
+    });
+
+    if (filterQuestion) {
+      return {
+        status: 200,
+        message: "Filter question added",
+        questions: filterQuestion.filterQuestions,
+      };
+    }
+    return {
+      status: 400,
+      message: "Oops! something went wrong",
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const onGetAllFilterQuestions = async (id: string) => {
+  console.log(id);
+  try {
+    const questions = await client.filterQuestions.findMany({
+      where: {
+        domainId: id,
+      },
+      select: {
+        question: true,
+        id: true,
+      },
+      orderBy: {
+        question: "asc",
+      },
+    });
+
+    return {
+      status: 200,
+      message: "",
+      questions: questions,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
